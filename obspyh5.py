97ef31f4eb7035d9deb6d996f65277a07dffeb8e
richter <richter@gfz-potsdam.de>
2013-12-02 18:19:20 +0100

    functional version, support writing into subgroups, recursive

--- obspyh5.py
+++ obspyh5.py
@@ -1,108 +1,116 @@
-from warnings import warn
 from numpy import string_
+from warnings import warn
 from obspy.core import Trace, Stream, UTCDateTime as UTC
+
 try:
-    from h5py import is_hdf5, File
+    import h5py
 except ImportError:
-    is_hdf5 = lambda x: False
-    File = object
-    
-_IGNORE_HEADERS = ('endtime', 'delta')    
-    
+    pass
+
+_IGNORE = ('endtime', 'delta')
+
+
 def _is_utc(utc):
     utc = str(utc)
     return len(utc) == 27 and utc.endswith('Z')
 
-def read(fname, headonly=False):
-    with StreamFile(fname, 'r') as f:
-        return f.read(headonly=headonly)
 
-def write(fname, stream, mode='w', ignore_headers=()):
+def is_hdf5(fname):
+    try:
+        return h5py.is_hdf5(fname)
+    except:
+        return False
+
+
+def read_hdf5(fname, group='/', **kwargs):
+    with h5py.File(fname, 'r') as f:
+        group = f.require_group(group)
+        return hdf2stream(group, **kwargs)
+
+
+def write_hdf5(fname, stream, mode='w', group='/', **kwargs):
     if not fname.endswith('.hdf5'):
-        fname = fname + '.hdf5' 
-    with StreamFile(fname, mode) as f:
-        f.extend(stream, ignore_headers=ignore_headers)
+        fname = fname + '.hdf5'
+    with h5py.File(fname, mode, libver='latest') as f:
+        group = f.require_group(group)
+        stream2hdf(group, stream, **kwargs)
 
-class StreamFile(File):
-    """
-    Supports partial reading, appending and deleting of Streams in hdf5 files.
-    """ 
-    def read(self, **kwargs):
-        return self.__getitem__(slice(None), **kwargs)
-    
-    def settrace(self, trace, key=None, ignore_headers=(), warn_override=True, **kwargs):
-        ignore_headers = tuple(ignore_headers) + _IGNORE_HEADERS
-        if key is None:
-            key = tr.id
-        if key in self:
-            if warn_override:
-                warn('Key %s already existing. Will override trace.' % key)
-            del self[key]
-        dat = self.create_dataset(key, trace.data.shape, trace.data.dtype, **kwargs)
-        dat[:] = trace.data
-        for key, val in trace.stats.items():
-            if key not in ignore_headers:
-                if isinstance(val, basestring) or _is_utc(val):
-                    dat.attrs[key] = string_(val)
-                else:
-                    try:
-                        dat.attrs[key] = val
-                    except TypeError:
-                        warn(("Writing header '%s' is not supported with h5py. " 
-                              "Only h5py types and UTCDateTime are supported.") % key)
-                              
-    def gettrace(self, key, headonly=False):
-        dat = self[key]
-        stats = dict(dat.attrs)
-        for key2, val in stats.items():
-            if _is_utc(val):
-                stats[key2] = UTC(val)
-        if headonly:
-            tr = Trace(header=stats)
-        else:
-            tr = Trace(data=dat[...], header=stats)
-        return tr
 
-    def append(self, trace, **kwargs):
-        try:
-            N = max(int(key) for key in self)+1
-        except:                
-            N = len(self)
-        self.settrace(trace, key=str(N), **kwargs)            
-            
-    def extend(self, stream, **kwargs):
-        try:
-            N = max(int(key) for key in self)+1
-        except:                
-            N = len(self)
-        for i, tr in enumerate(stream):
-            self.settrace(tr, key=str(i+N), **kwargs)
-        
-    def __getitem__(self, index, **kwargs):
-        if isinstance(index, int):
-            return self.gettrace(self.keys()[index], **kwargs)
-        elif isinstance(index, slice):
-            traces = [self.gettrace(key, **kwargs) for key in self.keys()[index]]
-            return Stream(traces=traces)
-        else:
-            return super(self.__class__, self).__getitem__(index, **kwargs)
+def trace2dataset(dataset, trace, ignore=()):
+    """Write trace.data and trace.stats into dataset"""
+    ignore = tuple(ignore) + _IGNORE
+    dataset[:] = trace.data
+    for key, val in trace.stats.items():
+        if key not in ignore:
+            if isinstance(val, basestring) or _is_utc(val):
+                dataset.attrs[key] = string_(val)
+            else:
+                try:
+                    dataset.attrs[key] = val
+                except TypeError:
+                    warn(("Writing header '%s' is not supported. Only h5py "
+                          "types and UTCDateTime are supported.") % key)
 
-    def __setitem__(self, index, val, **kwargs):
-        if isinstance(index, int):
-            self.settrace(val, key=self.keys()[index], warn_override=False, **kwargs)
-        elif isinstance(index, slice):
-            for key, v in zip(self.keys()[index], val):
-                self.settrace(v, key=key, warn_override=False, **kwargs)
-        else:
-            super(self.__class__, self).__setitem__(index, val, **kwargs)
-        
-    def __delitem__(self, index):
-        if isinstance(index, int):
-            del self[keys()[index]]
-        elif isinstance(index, slice):
-            for key in self.keys()[index]:
-                del self[key]
-        else:
-            return super(self.__class__, self).__delitem__(index)        
-  
-__all__ = [read, write, is_hdf5, StreamFile]         
+
+def trace2hdf(group, trace, key=None, override='warn', ignore=(), **kwargs):
+    """Write trace into group"""
+    if override not in ('warn', 'raise', 'ignore', 'dont'):
+        msg = "Override has to be one of ('warn', 'raise', 'ignore', 'dont')."
+        raise ValueError(msg)
+    key = key or trace.id
+    if key in group:
+        msg = "Key '%s' already exists." % key
+        if override == 'warn':
+            warn(msg + ' Will override trace.')
+        elif override == 'raise':
+            raise KeyError(msg)
+        elif override == 'dont':
+            return
+        del group[key]
+    dataset = group.create_dataset(key, trace.data.shape, trace.data.dtype,
+                                   **kwargs)
+    trace2dataset(dataset, trace, ignore=ignore)
+
+
+def stream2hdf(group, stream, indexing='num', **kwargs):
+    """Write stream into group"""
+    if indexing == 'num':
+        Ns = [0]
+        for k in group:
+            try:
+                Ns.append(int(k) + 1)
+            except ValueError:
+                pass
+        N = max(Ns)
+    elif indexing != 'id':
+        raise ValueError("Indexing has to be one of ('num', 'id').")
+    for i, tr in enumerate(stream):
+        key = str(N + i) if indexing == 'num' else None
+        trace2hdf(group, tr, key=key, **kwargs)
+
+
+def dataset2trace(dataset, headonly=False):
+    """Load trace from dataset"""
+    stats = dict(dataset.attrs)
+    for key, val in stats.items():
+        if _is_utc(val):
+            stats[key] = UTC(val)
+    if headonly:
+        tr = Trace(header=stats)
+    else:
+        tr = Trace(data=dataset[...], header=stats)
+    return tr
+
+
+def hdf2stream(group, recursive=False, headonly=False):
+    """Load stream from group"""
+    def _helper(g):
+        for key, val in g.items():
+            if isinstance(val, h5py.Dataset):
+                tr = dataset2trace(val, headonly=headonly)
+                traces.append(tr)
+            elif recursive:
+                _helper(val)
+    traces = []
+    _helper(group)
+    return Stream(traces=traces)
--- test_obspyh5.py
+++ test_obspyh5.py
@@ -1,83 +1,95 @@
 import unittest
 import os
 from tempfile import gettempdir
+import warnings
 
-from obspy import read
+from obspy import read as read_example
 from obspy.core import UTCDateTime as UTC
+from obspyh5 import (read_hdf5, write_hdf5, stream2hdf, hdf2stream, trace2hdf,
+                     dataset2trace)
 import obspyh5
+import h5py
+import numpy as np
 
-class ObsPyH5TestCase(unittest.TestCase):
-    
+
+class HDF5TestCase(unittest.TestCase):
+
     def test_is_utc(self):
         self.assertTrue(obspyh5._is_utc(UTC()))
         self.assertFalse(obspyh5._is_utc(110))
-        
-    def test_obspyh5(self):
-        fname = os.path.join(gettempdir(), 'test.hdf5')
-        stream=read()
-        stream[0].stats.onset = UTC()
-        stream[0].stats.crazyheader = 11.5
-        stream[0].stats.crazyheader2 = 'HEY'
-        
-        # write stream and read again, append data
-        obspyh5.write(fname, stream)
-        self.assertTrue(obspyh5.is_hdf5(fname))
-        stream2 = obspyh5.read(fname)
-        obspyh5.write(fname, stream, mode='a')
-        stream3 = obspyh5.read(fname)
-        self.assertEqual(stream, stream2)
-        self.assertEqual(stream+stream, stream3)
-        
-        # read only header
-        stream3 = obspyh5.read(fname, headonly=True)
-        self.assertEqual(stream2[0].stats, stream3[0].stats)
-        self.assertEqual(len(stream3[0].data), 0)
-       
-        os.remove(fname)
-        
-        # create hdf5 file object, extend f twice with stream
-        # read stream with slices, delete and overwrite some traces
-        # and see that all makes sense
-        with obspyh5.StreamFile(fname) as f:
-            f.extend(stream)
-            f.extend(stream)
-            
-            self.assertEqual(f[1], stream[1])
-            self.assertEqual(f[-3], stream[0])
-            self.assertEqual(f[:4]+f[4:], stream+stream)
-            self.assertEqual(len(f[:4]), 4)
-            self.assertEqual(len(f[6:10]), 0)
-            self.assertEqual(len(f[:-1]), 5)
-            
-            del f['2']
-            del f[0:2]
-            self.assertEqual(f[:], stream)
-            f[:2] = stream[:2]
-            f[-1] = stream[0]
-        with obspyh5.StreamFile(fname) as f:
-            self.assertEqual(f[:2], stream[:2])
-            self.assertEqual(f[-1], stream[0])
-            
-          
-            #stream[0].stats.stillok = [[3, 5, 6], [3, 2, 1]]
-            #stream[0].stats.convtostr = {'hey':3, 'ho':1}.items()
-            #stream[0].stats.toomuch={5:4}
-            #f.append(stream[1])
-            #f.append(stream[0])
-            #print f[-1].stats
-            #f.extend(stream, use_id=True)
-            #def func(name, obj):
-               #print name, type(obj)
-            #f.visititems(func)
-               
-        
-    
-def suite():
-    return unittest.makeSuite(ObsPyH5TestCase, 'test')
 
-def main():
-    unittest.main(defaultTest='suite')
-    
+    def test_obspyh5(self):
+        def subtest(fname, group='/'):
+            # write_hdf5 stream and read_hdf5 again, append data
+            write_hdf5(fname, stream, group=group)
+            self.assertTrue(obspyh5.is_hdf5(fname))
+            stream2 = read_hdf5(fname, group=group)
+            write_hdf5(fname, stream, mode='a', group=group)
+            stream3 = read_hdf5(fname, group=group)
+            self.assertEqual(stream[1:], stream2[1:])
+            self.assertEqual(stream + stream, stream3)
+            # read_hdf5 only header
+            stream3 = read_hdf5(fname, group=group, headonly=True)
+            self.assertEqual(stream2[0].stats, stream3[0].stats)
+            self.assertEqual(len(stream3[0].data), 0)
+            # test if group was really created
+            with h5py.File(fname) as f:
+                self.assertTrue(group in f)
+                self.assertTrue(len(f[group]) == 6)
+            # test numpy headers, check for warning
+            stream[0].stats.num = np.array([[5, 4, 3], [1, 2, 3.]])
+            stream[0].stats.toomuch = {1: 3}
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                write_hdf5(fname, stream, group=group)
+                self.assertEqual(len(w), 1)
+            stream2 = read_hdf5(fname, group=group)
+            np.testing.assert_array_equal(stream[0].stats.num,
+                                          stream2[0].stats.num)
+            del stream[0].stats.num
+            del stream[0].stats.toomuch
+            os.remove(fname)
+        stream = read_example()
+        stream[0].stats.onset = UTC()
+        stream[0].stats.header = 11.5
+        stream[0].stats.header2 = 'HEY'
+        fname1 = os.path.join(gettempdir(), 'test.hdf5')
+        # write into file
+        subtest(fname1)
+        # write into specific group
+        subtest(fname1, group='/traces/test')
+        # write with id indexing, order is not preserved
+        write_hdf5(fname1, stream, indexing='id')
+        stream2 = read_hdf5(fname1)
+        self.assertEqual(stream, stream2[::-1])
+        os.remove(fname1)
+
+        # use direct interface
+        with h5py.File(fname1) as f:
+            stream2hdf(f, stream)
+            trace2hdf(f, stream[0], key=None)  # key = trace.id
+            self.assertEqual(dataset2trace(f[stream[0].id]), stream[0])
+            # test override
+            with warnings.catch_warnings(record=True) as w:
+                warnings.simplefilter("always")
+                trace2hdf(f, stream[0], key=None)
+                self.assertEqual(len(w), 1)
+            with self.assertRaises(KeyError):
+                trace2hdf(f, stream[0], key=None, override='raise')
+            self.assertFalse(obspyh5.is_hdf5(f))  # only fnames can be checked
+            stream2hdf(f.create_group('test'), stream[-1:])
+            # test that subgroup does not interfere
+            N = len(hdf2stream(f))
+            self.assertEqual(N, 4)
+            # test recursive option
+            self.assertEqual(len(hdf2stream(f, recursive=True)), N + 1)
+            # test num indexing if not all entries are nums
+            stream2hdf(f, stream[-1:])
+
+
+def suite():
+    return unittest.makeSuite(HDF5TestCase, 'test')
+
+
 if __name__ == '__main__':
-    main()
-    
+    unittest.main(defaultTest='suite')
