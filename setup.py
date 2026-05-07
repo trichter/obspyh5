3c3d3e3c7e0288c9187f89a05bbb6a40276bf0fb
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2026-05-07 13:19:23 +0200

    update py classifiers

--- setup.py
+++ setup.py
@@ -33,12 +33,6 @@ CLASSIFIERS = [
     'Intended Audience :: Science/Research',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering :: Physics'
     ]
 
